24fcce54618200220b080a0f02b8a73913d046eb
Manuel Fontenelle <manuel.fontenelle@gmail.com>
2022-02-16 19:09:18 +0100

    clean code

--- routes/payment.js
+++ routes/payment.js
@@ -1,8 +1,6 @@
 const express = require("express")
 const router = express.Router()
 
-// router.use(formidableMiddleware());
-
 const stripe = require("stripe")(process.env.STRIPE_API_SECRET)
 
 router.post("/payment", async (req, res) => {
